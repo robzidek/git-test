f09328811f5f35b3835f158d5a06f1f3ab1356ff
Robert <robzidek@umich.edu>
2017-10-24 11:22:37 -0400

    adding another python test file

---
+++ lin_interp.py
@@ -0,0 +1,6 @@
+print("model: ax+b")
+a = float(input("enter a: "))
+b = float(input("enter b: "))
+x = float(input ("enter x: "))
+print('a={}, b={}, x={}'.format(a,b,x))
+print('interp value = {0}'.format(a*x+b))
